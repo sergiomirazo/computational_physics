6eee6a602f6862ce7c2f4847d5627ca576fbad54
Sergio Mirazo <106106677+sergiomirazo@users.noreply.github.com>
2023-05-14 17:12:15 -0700

    Update particulas.py

--- particulas.py
+++ particulas.py
@@ -1,50 +1,36 @@
 import tkinter as tk
 import random
 
-# configurar la ventana
-ventana = tk.Tk()
-ventana.geometry("500x500")
-ventana.title("Simulación de Partículas")
-canvas = tk.Canvas(ventana, bg="black", width=500, height=500)
+window = tk.Tk()
+window.geometry("500x500")
+window.title("Particles simulation")
+canvas = tk.Canvas(window, bg="black", width=500, height=500)
 canvas.pack()
 
-# modificar los parámetros aquí
-num_particulas = 100
-tamaño_particula = 10
+num_particles = 100
+size_particle = 10
 
-# crear las partículas
-particulas = []
-for i in range(num_particulas):
-    # generar coordenadas aleatorias para cada partícula
+particles = []
+for i in range(num_particles):
     x = random.randint(20, 480)
     y = random.randint(20, 480)
-    # generar velocidades aleatorias para cada partícula
     vx = random.randint(-5, 5)
     vy = random.randint(-5, 5)
-    # crear la partícula
-    particulas.append([x, y, vx, vy])
+    particles.append([x, y, vx, vy])
 
-# actualizar el canvas
-def actualizar():
-    # mover las partículas
-    for p in particulas:
+def refresh():
+    for p in particles:
         p[0] += p[2]
         p[1] += p[3]
-        # comprobar si la partícula sale de los límites
         if p[0] > 480 or p[0] < 20:
             p[2] *= -1
         if p[1] > 480 or p[1] < 20:
             p[3] *= -1
-    # borrar el canvas
     canvas.delete("all")
-    # dibujar las partículas
-    for p in particulas:
-        canvas.create_oval(p[0], p[1], p[0] + tamaño_particula, p[1] + tamaño_particula, fill="green")
-    # llamar recursivamente a la función para actualizar el canvas
-    ventana.after(10, actualizar)
+    for p in particles:
+        canvas.create_oval(p[0], p[1], p[0] + size_particle, p[1] + size_particle, fill="green")
+    window.after(10, refresh)
 
-# llamada para inicializar el canvas
-actualizar()
+refresh()
 
-# iniciar la ventana
-ventana.mainloop()
+window.mainloop()
